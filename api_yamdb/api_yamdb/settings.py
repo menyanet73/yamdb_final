4a82bd84b4b2df88ede88825ae8e600daca4078d
Maxim <menyanet73@gmail.com>
2022-05-23 19:56:56 +0300

    settings fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST', default='web')]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST', default='*')]
 
 
 # Application definition
